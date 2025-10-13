c356ca41713720bfb980d862748a3eb20bbe7a30
Khawar Mehfooz <khawarmehfooz565@gmail.com>
2025-10-13 12:46:06 +0500

    add unique check on donor information

--- app/Filament/Resources/Donors/Schemas/DonorForm.php
+++ app/Filament/Resources/Donors/Schemas/DonorForm.php
@@ -17,21 +17,34 @@ class DonorForm
                         TextInput::make('donor_name')
                             ->label('Full Name')
                             ->required()
+                            ->autocomplete(false)
                             ->placeholder('Donor\'s Name')
                             ->prefixIcon('heroicon-o-user'),
 
                         TextInput::make('donor_cnic')
                             ->unique(table: 'donors', column: 'donor_cnic')
                             ->label('Donor\'s CNIC')
-                            ->mask('12345-6789123-4')
+                            ->autocomplete(false)
+                            ->mask('99999-9999999-9')
                             ->placeholder('12345-6789123-4')
+                            ->unique(
+                                ignoreRecord: true, 
+                                table: 'donors', 
+                                column: 'donor_cnic'
+                            )
                             ->prefixIcon('heroicon-o-identification'),
 
                         TextInput::make('donor_contact_number')
                             ->label('Donor\'s Contact Number')
                             ->mask('9999-9999999')
+                            ->autocomplete(false)
                             ->placeholder('0300-1234567')
-                            ->prefixIcon('heroicon-o-phone'),
+                            ->prefixIcon('heroicon-o-phone')
+                            ->unique(
+                                ignoreRecord: true, 
+                                table: 'donors', 
+                                column: 'donor_contact_number'
+                            ),
 
                     ])
                     ->columnSpanFull()
